e3822b68a0528f49a7b3ee70eac464c6276ae850
d2k11 <65378620+d2k11@users.noreply.github.com>
2024-07-25 21:39:23 -0500

    update main program

--- Conduit/Program.cs
+++ Conduit/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Data;
 using System.Net;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Conduit.Models;
 
 namespace Conduit
 {
@@ -14,48 +16,19 @@ namespace Conduit
         static async Task Main(string[] args)
         {
             string path = Environment.UserName != "root" ? "conduit.conf" : "/root/Conduit/conduit.conf";
-            
-            if (!File.Exists(path))
-            {
-                File.Create(path).Close();
-                File.WriteAllText(path, "# Conduit Configuration File #\n\nALIAS conduit-test AS conduit-test.blockbase.gg:3139\n\nFORWARD 8888 TO conduit-test");
-            }
-            string[] lines = File.ReadAllLines(path);
-
-            ReadWhitelist();
+            RuleManager.Load();
 
             Console.WriteLine("===================================");
-            Dictionary<string, string> aliases = new();
-            foreach (string line in lines)
+            foreach (ConduitRule rule in RuleManager.Rules)
             {
-                if(string.IsNullOrEmpty(line) || line.StartsWith("#"))
+                if (rule.Verb == ConduitVerb.FORWARD)
                 {
-                    continue;
-                }
-                string[] parts = line.Split(' ');
-                // ALIAS conduit-test AS conduit-test.blockbase.gg:3139
-                // FORWARD 8888 TO conduit-test.blockbase.gg:3139
-                string verb = parts[0];
-                string local = parts[1];
-                string remote = parts[3];
-
-                if (verb == "FORWARD")
-                {
-                    int remotePort = remote.Split(':').Length > 1 ? int.Parse(remote.Split(':')[1]) : 0;
-                    string remoteHost = remote.Split(':')[0];
-                    if (!IsValidIPv4(remoteHost))
-                    {
-                        if (aliases.ContainsKey(remoteHost))
-                        {
-                            string newHost = aliases[remoteHost].Split(':')[0];
-                            remotePort = int.Parse(aliases[remoteHost].Split(':')[1]);
-                            remoteHost = newHost;
-                        }
-                    }
-                    int localPort = int.Parse(local);
+                    int localPort = int.Parse(rule.Data.Split("|")[0]);
+                    int remotePort = int.Parse(rule.Data.Split("|")[1].Split(':')[1]);
+                    string remoteHost = rule.Data.Split("|")[1].Split(':')[0];
                     var listener = new TcpListener(IPAddress.Any, localPort);
                     listener.Start();
-                    Console.WriteLine($"localhost:{localPort} -> {remoteHost}:{remotePort}");
+                    Console.WriteLine($"{localPort} -> {remoteHost}:{remotePort}");
                     Task task = new Task(async () =>
                     {
                         while (true)
@@ -67,12 +40,9 @@ namespace Conduit
                     threads.Add(task);
                     task.Start();
                 }
-                else if (verb == "ALIAS")
-                {
-                    aliases.Add(local, remote);
-                }
             }
             Console.WriteLine("===================================");
+            
             while (true)
             {
                 ConsoleKeyInfo key = Console.ReadKey();
@@ -100,10 +70,10 @@ namespace Conduit
                 string ip = client.Client.RemoteEndPoint.ToString().Split(':')[0];
 
                 Console.Write(DateTime.Now.ToLongTimeString() + ": " + ip+":"+localPort+" -> "+remoteHost+":"+remotePort);
-                ReadWhitelist();
+
                 string logPath = Environment.UserName != "root" ? "log.txt" : "/root/Conduit/log.txt";
                 if(!File.Exists(logPath)) File.Create(logPath).Close();
-                if ((!ports.ContainsKey(ip) || !ports[ip].Contains(localPort.ToString())) && !ports[ip].Contains("*"))
+                if (!RuleManager.Allow(ip, localPort) && !RuleManager.Allow(RuleManager.GetAlias(ip), localPort))
                 {
                     Console.WriteLine(" [DROPPED]");
                     File.WriteAllText(logPath, File.ReadAllText(logPath) +
@@ -111,15 +81,19 @@ namespace Conduit
                                                DateTime.Now.ToLongTimeString() + ": " + ip + ":" + localPort + " -> " +
                                                remoteHost + ":" +
                                                remotePort + " [DROPPED]\n");
-                    return;
+                    remoteHost = "1.1.1.1";
+                    remotePort = 22;
+                }
+                else
+                {
+                    Console.WriteLine(" [ACCEPTED]");
+                    File.WriteAllText(logPath, File.ReadAllText(logPath) +
+                                               DateTime.Now.ToShortDateString() + " " +
+                                               DateTime.Now.ToLongTimeString() + ": " + ip + ":" + localPort + " -> " +
+                                               remoteHost + ":" +
+                                               remotePort + " [ACCEPTED]\n");
                 }
 
-                Console.WriteLine(" [ACCEPTED]");
-                File.WriteAllText(logPath, File.ReadAllText(logPath) +
-                                           DateTime.Now.ToShortDateString() + " " +
-                                           DateTime.Now.ToLongTimeString() + ": " + ip + ":" + localPort + " -> " +
-                                           remoteHost + ":" +
-                                           remotePort + " [ACCEPTED]\n");
                 var remoteClient = new TcpClient();
                 await remoteClient.ConnectAsync(remoteHost, remotePort);
 
@@ -145,73 +119,6 @@ namespace Conduit
             {
                 await output.WriteAsync(buffer, 0, bytesRead);
             }
-        }
-
-        private static void ReadWhitelist()
-        {
-            ports = new();
-            string path = Environment.UserName != "root" ? "whitelist.conf" : "/root/Conduit/whitelist.conf";
-            if (!File.Exists(path))
-            {
-                File.Create(path).Close();
-                File.WriteAllText(path,
-                    "# Conduit Whitelist File #\n\nALIAS localhost AS 127.0.0.1\n\nALLOW 127.0.0.1 TO 8888");
-            }
-
-            List<string> whitelist = File.ReadAllLines(path).ToList();
-            Dictionary<string, string> aliases = new();
-            foreach (string entry in whitelist)
-            {
-                if (string.IsNullOrEmpty(entry) || entry.StartsWith("#"))
-                {
-                    continue;
-                }
-                
-                // ALLOW 1.1.1.1 TO 8888
-                string[] parts = entry.Split(' ');
-                string verb = parts[0];
-                string ip = parts[1];
-                if (!IsValidIPv4(ip))
-                {
-                    if (ip == "ALL")
-                    {
-                        ip = "*";
-                    }
-                    if (aliases.ContainsValue(ip))
-                    {
-                        ip = aliases.Where(alias => alias.Value == ip).First().Key;
-                    }
-                }
-                string port = parts[3];
-                if (verb == "ALLOW")
-                {
-                    if (!ports.ContainsKey(ip))
-                    {
-                        ports.Add(ip, port);
-                    }
-                    else
-                    {
-                        ports[ip] += ", " + port;
-                    }
-                }
-                else if (verb == "DENY")
-                {
-                    if (ports.ContainsKey(ip))
-                    {
-                        ports[ip] = ports[ip].Replace(port, "");
-                    }
-                }
-                else if (verb == "ALIAS")
-                {
-                    aliases.Add(port, ip);
-                }
-            }
-        }
-        
-        static bool IsValidIPv4(string ipString)
-        {
-            string ipv4Pattern = @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$";
-            return Regex.IsMatch(ipString, ipv4Pattern);
         }
     }
 }
